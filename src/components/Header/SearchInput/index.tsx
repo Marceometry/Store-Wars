84ccb04b26947084a30891990c63ab303494eefe
Marcelino Teixeira <marceometry@gmail.com>
2021-08-05 09:31:50 -0300

    add clear input function

--- src/components/Header/SearchInput/index.tsx
+++ src/components/Header/SearchInput/index.tsx
@@ -1,45 +1,48 @@
-// import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { Close } from "../../../svg/Close"
 import { Search } from "../../../svg/Search"
 import styles from "./style.module.scss"
 
 export default function SearchInput() {
-  // const inputRef = useRef<HTMLInputElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [value, setValue] = useState('')
   // const { searchText, setSearchText, setIsInputEmpty, debouncedSearch, setIsLoading } = useSearch()
 
-  // useEffect(() => {
-  //   inputRef.current && inputRef.current.focus()
-  // }, [])
+  useEffect(() => {
+    inputRef.current && inputRef.current.focus()
+  }, [])
 
-  // const handleChange = (value: string) => {
-    // setSearchText(value)
+  const handleChange = (value: string) => {
+    setValue(value)
     // if (value === '') {
-    //   setIsInputEmpty(true)
-    //   setIsLoading(false)
+      // setIsInputEmpty(true)
+      // setIsLoading(false)
     //   return
     // }
     // setIsLoading(true)
     // debouncedSearch(value)
-  // }
+  }
 
-  // function clearInput() {
-  //   inputRef.current.value = ''
-  //   inputRef.current.focus()
-  //   setIsInputEmpty(true)
-  //   setSearchText('')
-  // }
+  function clearInput() {
+    inputRef.current?.focus()
+    // setIsInputEmpty(true)
+    setValue('')
+  }
 
   return (
     <fieldset className={styles.searchInput}>
-      <input
-      // ref={inputRef}
+      <input ref={inputRef} value={value} autoFocus
         type="text" spellCheck="false" aria-label="Buscar"
-        maxLength={80} autoCorrect="off" name="buscar" autoFocus
+        maxLength={80} autoCorrect="off" name="buscar"
         placeholder="Buscar produtos..."
-        // value={searchText}
-        // onChange={() => handleChange(inputRef.current.value)}
+        onChange={e => handleChange(e.target.value)}
       />
 
-      <Search />
+      {!value ? <Search /> : (
+        <button onClick={clearInput} aria-label="Limpar pesquisa">
+          <Close />
+        </button>
+      )}
     </fieldset>
   )
 }
