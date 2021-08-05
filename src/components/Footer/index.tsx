d4502d16a8367cace378651badecbc8efd7104e4
Marcelino Teixeira <marceometry@gmail.com>
2021-08-05 09:55:35 -0300

    fix footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,14 @@
 import style from './style.module.scss'
 
 export function Footer() {
-    return (
-        <footer className={style.footer}>
-            <p>Feito com A Força por <a href="https://github.com/Marceometry">Marcelino Teixeira</a></p>
-        </footer>
-    )
+  return (
+    <footer className={style.footer}>
+      <p>
+        Feito com A Força por <a href="https://github.com/Marceometry"
+                                 rel="noopener noreferrer nofollow" target="_blank" >
+                                 Marcelino Teixeira
+                              </a>
+      </p>
+    </footer>
+  )
 }
