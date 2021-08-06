a8ac2f7f8eac9fcc4e363f911f88cacc21dd4a8a
Marcelino Teixeira <marceometry@gmail.com>
2021-08-05 21:15:14 -0300

    style categories aside

--- src/components/Search/Filters/index.tsx
+++ src/components/Search/Filters/index.tsx
@@ -1,9 +1,35 @@
 import style from './style.module.scss'
 
+const categories = [
+    { name: "Jedi" },
+    { name: "Sith" },
+    { name: "Legos" },
+    { name: "Brinquedos" },
+    { name: "Camisas" },
+]
+
 export function Filters() {
     return (
         <aside className={style.container}>
             <h1>Filtros:</h1>
+
+            <div className={style.filters}>
+                <h1>Categorias</h1>
+
+                {categories.map(category => (
+                    <fieldset>
+                        <input
+                            name={category.name}
+                            type="checkbox"
+                            // checked={isChecked}
+                            // onChange={e => handleChange(e.target.checked)}
+                        />
+                        <label htmlFor={category.name}>
+                            {category.name}
+                        </label>
+                    </fieldset>
+                ))}
+            </div>
         </aside>
     )
 }
