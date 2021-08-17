535fa817ea08881f57fd5576530af15999cfae0a
Marcelino <marceometry@gmail.com>
2021-08-16 23:05:32 -0300

    use api in cart page

--- src/components/Purchase/CartListItem/index.tsx
+++ src/components/Purchase/CartListItem/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import { useEffect, useState } from "react"
 import { usePurchase } from "../../../contexts/PurchaseContext"
-import { Product } from "../../../data/products"
+import { Product } from "../../../utils/productType"
 import { Quantity } from "../Quantity"
 
 import style from "./style.module.scss"
@@ -15,14 +15,14 @@ export function CartListItem({ product }: CartListItemProps) {
     const [quantity, setQuantity] = useState(product.quantity ?? 1)
 
     useEffect(() => {
-        changeProductQuantity(product.id, quantity)
+        changeProductQuantity(product._id, quantity)
     }, [quantity])
 
     return (
         <li className={style.li}>
             <div className={style.img}>
                 <Image width={160} height={160} alt={product.name}
-                    src={`/images/products/${product.id}/${product.images[0]}`}
+                    src={`/images/products/${product._id}/${product.images[0]}`}
                 />
             </div>
             <div className={style.productInfo}>
@@ -38,7 +38,7 @@ export function CartListItem({ product }: CartListItemProps) {
                 <h2>R${product.price.toLocaleString('pt-BR')}</h2>
 
                 <div className={style.buttons}>
-                    <button onClick={() => removeProductFromCart(product.id)}>Remover</button>
+                    <button onClick={() => removeProductFromCart(product._id)}>Remover</button>
                 </div>
             </div>
         </li>
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,40 +1,47 @@
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
 
+import { useQuery } from '@apollo/client'
+import { GET_PRODUCTS } from '../services/graphql/getProducts'
+
 import { usePurchase } from '../contexts/PurchaseContext'
 import { NotFoundMessage } from '../components/NotFoundMessage'
 import { PurchaseInfo } from '../components/Purchase/PurchaseInfo'
 import { CartListItem } from '../components/Purchase/CartListItem'
 import { StyledButton, StyledLink } from '../components/LinkButton'
-import products, { Products } from '../data/products'
+
+import { Product } from '../utils/productType'
 
 import style from '../styles/cartPage.module.scss'
 
 export default function Cart() {
+    const { data, error, loading } = useQuery(GET_PRODUCTS)
     const { productsInCart } = usePurchase()
-    const [productsList, setProductsList] = useState([] as Products)
+    const [productsList, setProductsList] = useState([] as Product[])
     const [totalPrice, setTotalPrice] = useState(0)
 
     useEffect(() => {
-        const productsListArray = products.filter(product => {
+        if (error || loading) return
+
+        const productsListArray = data.products.filter((product: Product) => {
             const isInCart = productsInCart.filter(productInCart => {
-                return product.id === productInCart.id
+                return product._id === productInCart.id
             })
             
             return isInCart[0]
         })
 
-        productsListArray.forEach(product => {
-            productsInCart.forEach(productInCart => {
-                if (product.id !== productInCart.id) return
+        // productsListArray.forEach((product: Product) => {
+        //     productsInCart.forEach(productInCart => {
+        //         if (product._id !== productInCart.id) return
                 
-                product['quantity'] = productInCart.quantity
-                return
-            })
-        })
+        //         product['quantity'] = productInCart.quantity
+        //         return
+        //     })
+        // })
 
         setProductsList(productsListArray)
-    }, [productsInCart])
+    }, [productsInCart, data])
     
     useEffect(() => {
         const totalPrice = productsList.reduce((total, product) => {
@@ -55,13 +62,15 @@ export default function Cart() {
 
             <div className={style.cart}>
                 <ul className={productsList.length === 0 ? style.emptyCart : ''}>
-                    {productsList.length === 0 ? (
+                    {loading ? (
+                        <h1>Carregando...</h1>
+                    ) : productsList.length === 0 ? (
                         <NotFoundMessage message="O carrinho está vazio" />
                     ) : (
                         productsList.map(product => (
-                            <CartListItem key={product.id} product={product} />
-                        )
-                    ))}
+                            <CartListItem key={product._id} product={product} />
+                        ))
+                    )}
                 </ul>
 
                 <PurchaseInfo price={totalPrice}>
